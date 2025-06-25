1b4a0d61da5e9b92fbc993cb4baec8e9a4031a0b
sham-sha <sham08092003@gmail.com>
2025-06-25 22:18:56 +0530

    fib

---
+++ dsa3.js
@@ -0,0 +1,10 @@
+function fibbonacci(num) {
+  if (num <= 1) return num;
+  let a = 0, b = 1, c;
+  for (let i = 2; i <= num; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}       
