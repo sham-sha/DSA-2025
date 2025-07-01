ef7c57eda27de352fad0f3f0be61f16ba7263841
sham-sha <sham08092003@gmail.com>
2025-07-01 18:58:43 +0530

    changes

--- two.js
+++ two.js
@@ -4,3 +4,14 @@ function fibbonacci(n) {
 }
 
 fibbonacci(5); // 5
+
+function fibonacci(n) {
+  if (n <= 1) return n;
+  let a = 0, b = 1, c;
+  for (let i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
