26967d7257e1054f8a22b0c9cc0918f437660bd3
sham-sha <sham08092003@gmail.com>
2025-07-03 21:37:57 +0530

    changes

--- one.js
+++ one.js
@@ -1,1 +1,2 @@
-console.log("Hello, World!");           // This is a simple JavaScript program that prints "Hello, World!" to the console.
+console.log("Hello, World!");   
+console.log("bye world")        // This is a simple JavaScript program that prints "Hello, World!" to the console.
